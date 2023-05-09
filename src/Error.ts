2e37a60040eca689ec7bbcfa2e30baa324296c20
daijie <daijie@keqin.tech>
2023-05-09 08:40:15 +0000

    fix: close #5 compatible with BuyPass's error description

--- src/Error.ts
+++ src/Error.ts
@@ -5,26 +5,45 @@ export class ErrorNotDiscovered extends Error {}
 export interface ErrorDescription {
     type: string
     detail: string
-    status: number
+    /**
+     * status is not required in ACME (rfc8555)
+     * Some Authority may not use `status` field
+     * e.g. BuyPass use `code` 
+     */
+    status?: number
+    code?: number
 }
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function isErrorDescription(desc: any): desc is ErrorDescription {
     return (
         isObject(desc) &&
-        isString([desc.type, desc.detail]) &&
-        Number.isInteger(desc.status)
+        isString([desc.type, desc.detail])
     )
 }
 
 export class ErrorResponse extends Error {
     public type: string
-    public status: number
+    public status?: number
+    private desc: ErrorDescription
 
     constructor(desc: ErrorDescription) {
         super(desc.detail)
+        this.desc = desc
         this.type = desc.type
         this.status = desc.status
+    }
+
+    toJSON() {
+        return {
+            type: this.type,
+            message: this.desc.detail,
+            status: this.desc.status || this.desc.code
+        }
+    }
+
+    toString() {
+        return `[${this.type}] ${this.message}`
     }
 }
 
--- src/__tests__/error.spec.ts
+++ src/__tests__/error.spec.ts
@@ -8,7 +8,6 @@ test("isErrorDescription", () => {
     const desc = {
         type: "urn:ietf:params:acme:error:malformed",
         detail: "Some of the identifiers requested were rejected",
-        status: 403,
     }
     expect(isErrorDescription(desc)).toBeTruthy()
 
@@ -29,6 +28,33 @@ test("ErrorResponse", () => {
     expect(() => {
         throw new ErrorResponse(desc)
     }).toThrowError(desc.detail)
+    try {
+        throw new ErrorResponse(desc)
+    } catch (err) {
+        const errorString = err.toString()
+        const errorObj = err.toJSON()
+        expect(errorString).toContain(desc.type)
+        expect(errorString).toContain(desc.detail)
+        expect(errorObj.type).toBe(desc.type)
+        expect(errorObj.message).toBe(desc.detail)
+    }
+
+})
+test("BuyPass Error Code", () => {
+    // BuyPass error code compatible
+    const buyPassErrorDesc = {
+        type: 'urn:ietf:params:acme:error:malformed',
+        detail: 'Wildcard not supported, but *.any.host requested',
+        code: 400,
+        message: 'MALFORMED_BAD_REQUEST',
+        details: 'HTTP 400 Bad Request'
+    }
+    try {
+        throw new ErrorResponse(buyPassErrorDesc)
+    } catch (err) {
+        const errorObj = err.toJSON()
+        expect(errorObj.status).toBe(buyPassErrorDesc.code)
+    }
 })
 
 test("ErrorMalformedResponse", () => {
